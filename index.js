0afebf79ea370fd28574b9eb4bed12b14b0f7207
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-05-28 15:57:56 +0100

    setup project

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-import express, { response } from 'express';
-import dotenv from 'dotenv' 
-import cors from 'cors'
-import mongoose from 'mongoose'
-import connectDB from './db/connect.js';
-import errorHandler from './middlewares/error-handler.js';
-import notFound from './middlewares/not-found.js';
-import postRoutes from './routes/posts.js'
+import express from "express";
+import connectDB from './src/db/connect.js';
+import "express-async-errors"
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
+
 const app = express();
-app.use(cors())
 
 app.use(express.json())
-app.get('/api/v1', (req, res) => {
-    return res.send('This is the homepage of the api')
-})
-app.use('/api/v1/posts', postRoutes )
 
-app.use(notFound)
-app.use(errorHandler)
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('e-commerce api')
+})
 
 const start = async () => {
-    try {
-        // await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => {
-            console.log('Server is listening on port ' + port)
-        })
-    } catch (err) {
-        console.log(err)
-    }
-}
-mongoose.set('strictQuery', false) // to avoid console warnings and prepare for mongoose 7
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 start()
