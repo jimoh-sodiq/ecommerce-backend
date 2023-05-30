a2292222013cddc154c5b5f1b99d60e645478d38
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-05-30 18:06:34 +0100

    user registration endpoint implemented

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,17 @@
-import { StatusCodes } from 'http-status-codes';
+import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 import { createResponse } from "../utils/global.js";
 
 export async function register(req, res) {
   const { email, password, name } = req.body;
   const existingUser = await User.findOne({ email });
-  // if(existingUser){
-  //     return re
-  // }
-  res.status(200).json(req.body);
+  if (existingUser) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(createResponse(false, null, "Email already exists"));
+  }
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json(createResponse(true, { user }));
 }
 
 export async function login(req, res) {
