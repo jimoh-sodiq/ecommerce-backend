73cdcb73fbba1e27d8431c1ddfb5710a8abe3a2d
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-05-31 09:11:43 +0100

    register controller done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,11 +6,14 @@ export async function register(req, res) {
   const { email, password, name } = req.body;
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    res
+    return res
       .status(StatusCodes.BAD_REQUEST)
       .json(createResponse(false, null, "Email already exists"));
   }
-  const user = await User.create(req.body);
+
+  const userCount = await User.countDocuments({});
+  const role = userCount ? "user" : "admin";
+  const user = await User.create({ email, password, name, role });
   res.status(StatusCodes.CREATED).json(createResponse(true, { user }));
 }
 
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,39 +1,42 @@
 import mongoose from "mongoose";
 import validator from "validator";
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: "Invalid email format",
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
     },
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    validate: {
-      validator: function (value) {
-        // Password validation regex pattern (minimum 8 characters with at least one uppercase letter and one lowercase letter)
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-        return passwordRegex.test(value);
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Invalid email format",
       },
-      message:
-        "Password must contain at least 8 characters, including one uppercase letter and one lowercase letter",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      validate: {
+        validator: function (value) {
+          // Password validation regex pattern (minimum 8 characters with at least one uppercase letter and one lowercase letter)
+          const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+          return passwordRegex.test(value);
+        },
+        message:
+          "Password must contain at least 8 characters, including one uppercase letter and one lowercase letter",
+      },
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
     },
   },
-  role: {
-    type: String, 
-    enum: ['admin', 'user'],
-    default: 'user'
-  }
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 
