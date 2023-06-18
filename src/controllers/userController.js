b19bc9be470289311bf0e6129107cc07ca7a533b
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-06-18 17:38:02 +0100

    get all users and get single users implememnted

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,28 @@
+import { StatusCodes } from "http-status-codes";
+import User from "../models/userModel.js";
+import { BadRequest, NotFoundError } from "../errors/index.js";
+import {
+  createResponse,
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponse,
+} from "../utils/global.js";
+import jwt from "jsonwebtoken";
+
 export async function getAllUsers(req, res) {
-  res.send("get All users");
+  const users = await User.find({ role: "user" }).select("-password");
+  if (!users) {
+    throw new NotFoundError("No users found");
+  }
+  res.status(StatusCodes.OK).json(createResponse(true, { users }));
 }
 
 export async function getSingleUser(req, res) {
-  res.send("get single user");
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  if (!user) {
+    throw new NotFoundError("No user with this id");
+  }
+  res.status(StatusCodes.OK).json(createResponse(true, { userData: user }, "user found"));
 }
 
 export async function showCurrentUser(req, res) {
@@ -11,7 +30,7 @@ export async function showCurrentUser(req, res) {
 }
 
 export async function updateUser(req, res) {
-  res.send("update user");
+  res.send(req.body);
 }
 
 export async function updateUserPassword(req, res) {
