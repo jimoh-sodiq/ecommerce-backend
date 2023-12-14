fba7338c1237817dc2f5e3139a58eb852bf233bf
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-12-14 13:30:39 +0100

    populate user and product for a review

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,70 +1,86 @@
-import { StatusCodes } from 'http-status-codes'
-import Review from "../models/reviewModel.js"
-import { createResponse } from '../utils/global.js'
+import { StatusCodes } from "http-status-codes";
+import Review from "../models/reviewModel.js";
+import { createResponse } from "../utils/global.js";
 import Product from "../models/productModel.js";
-import BadRequest from '../errors/bad-request.js';
-import NotFoundError from '../errors/notfound-error.js';
-import { checkPermissions } from '../utils/checkPermissions.js';
-
-
+import BadRequest from "../errors/bad-request.js";
+import NotFoundError from "../errors/notfound-error.js";
+import { checkPermissions } from "../utils/checkPermissions.js";
 
 export async function createReview(req, res) {
+  const { product: productId } = req.body;
+  const isValidProduct = await Product.findOne({ _id: productId });
+  if (!isValidProduct) {
+    throw new BadRequest("product not found");
+  }
 
-    const {product: productId}  = req.body
-    const isValidProduct = await Product.findOne({_id: productId})
-    if(!isValidProduct){
-        throw new BadRequest("product not found")
-    }
-
-    const alreadyReviewed = await Review.findOne({product: productId, user:req.user.userId})
-    if(alreadyReviewed){ 
-        throw new BadRequest("already reviewed product")
-    }
-    req.body.user = req.user.userId
-    const review = await Review.create(req.body)
-    res.status(StatusCodes.CREATED).json(createResponse(true, {review}, "review created successfully"))
+  const alreadyReviewed = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+  if (alreadyReviewed) {
+    throw new BadRequest("already reviewed product");
+  }
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+  res
+    .status(StatusCodes.CREATED)
+    .json(createResponse(true, { review }, "review created successfully"));
 }
 
 export async function getAllReviews(req, res) {
-    const reviews = await Review.find({})
-    res.status(StatusCodes.OK).json(createResponse(true, {reviews,count: reviews.length}, "reviews fetched successfully"))
+  const reviews = await Review.find({})
+    .populate({ path: "product", select: "name company price" })
+    .populate({ path: "user", select: "name email" });
+  res
+    .status(StatusCodes.OK)
+    .json(
+      createResponse(
+        true,
+        { reviews, count: reviews.length },
+        "reviews fetched successfully"
+      )
+    );
 }
 
-
 export async function getSingleReview(req, res) {
-    const reviewId = req.params.id
-    const review  = await Review.findOne({_id: reviewId})
-    if(!review){
-        throw new NotFoundError("review not found")
-    }
-    res.status(StatusCodes.OK).json(createResponse(true, {review}, "review fetched successfully"))
+  const reviewId = req.params.id;
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
+    throw new NotFoundError("review not found");
+  }
+  res
+    .status(StatusCodes.OK)
+    .json(createResponse(true, { review }, "review fetched successfully"));
 }
 
 export async function updateReview(req, res) {
-    const {rating, comment, title} = req.body
-    const reviewId = req.params.id
-    const review  = await Review.findOne({_id: reviewId})
-    console.log(review)
-    if (!review) {
-        throw new NotFoundError("review not found")
-    }
-    checkPermissions(req.user, review.user)
-    review.comment = comment ?? review.comment
-    review.title = title ?? review.title
-    review.rating = rating ?? review.rating
-    await review.save()
-    return res.status(StatusCodes.OK).json(createResponse(true, {review}, "review updated successfully"))
+  const { rating, comment, title } = req.body;
+  const reviewId = req.params.id;
+  const review = await Review.findOne({ _id: reviewId });
+  console.log(review);
+  if (!review) {
+    throw new NotFoundError("review not found");
+  }
+  checkPermissions(req.user, review.user);
+  review.comment = comment ?? review.comment;
+  review.title = title ?? review.title;
+  review.rating = rating ?? review.rating;
+  await review.save();
+  return res
+    .status(StatusCodes.OK)
+    .json(createResponse(true, { review }, "review updated successfully"));
 }
 
 export async function deleteReview(req, res) {
-    const reviewId = req.params.id
-    const review  = await Review.findOne({_id: reviewId})
-    if(!review){
-        throw new NotFoundError("review not found")
-    }
-    checkPermissions(req.user, review.user)
-    await review.remove()
-    
-    res.status(StatusCodes.OK).json(createResponse(true, {review}, "review deleted successfully"))
+  const reviewId = req.params.id;
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
+    throw new NotFoundError("review not found");
+  }
+  checkPermissions(req.user, review.user);
+  await review.remove();
 
+  res
+    .status(StatusCodes.OK)
+    .json(createResponse(true, { review }, "review deleted successfully"));
 }
