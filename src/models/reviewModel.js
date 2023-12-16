c72ee9eff19c5134a7bdfdcc21679908d0048f32
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-12-16 14:54:07 +0100

    setup aggregation pipeline for product average rating and num of reviews

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -39,6 +39,46 @@ const ReviewSchema = new mongoose.Schema(
 // a user can only create one review per product
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true })
 
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const aggregateResult  = await this.aggregate([
+    {
+      $match: {
+        'product': productId
+      },
+    },
+    {
+      '$group': {
+        '_id': null,
+        'averageRating': {
+          '$avg': '$rating'
+        },
+        'numOfReviews': {
+          '$sum': 1
+        }
+      }
+    }
+  ])
+  
+ try{
+  await this.model('Product').findOneAndUpdate({_id: productId}, {
+    averageRating: Math.ceil(aggregateResult[0]?.averageRating || 0),
+    numOfReviews: aggregateResult[0]?.numOfReviews || 0,
+  })
+ } catch(error) {
+  console.log(error)
+ }
+}
+
+ReviewSchema.post('save', async function (next){
+  await this.constructor.calculateAverageRating(this.product)
+   console.log("saved a review")
+})
+
+ReviewSchema.post('remove', async function (next){
+  await this.constructor.calculateAverageRating(this.product)
+   console.log("removed a review")
+})
+
 const Review = mongoose.model("Review", ReviewSchema);
 
 export default Review;
