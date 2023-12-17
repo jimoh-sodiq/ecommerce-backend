8dc2c692e37a14466f7ed137bcaa1641c9f68b6b
jimoh-sodiq <jimohsodiq301@gmail.com>
2023-12-17 08:51:56 +0100

    create order

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,12 +1,64 @@
-import Order from "../models/orderModel.js";
+import { Order, SingleOrderItem } from "../models/orderModel.js";
+import Product from "../models/productModel.js";
 import { StatusCodes } from "http-status-codes";
 import { BadRequest, NotFoundError } from "../errors/index.js";
 import { createResponse } from "../utils/global.js";
 
-export async function createOrder(req, res) {
+const fakeStripeApi = async ({ amount, currency }) => { 
+  const clientSecret = "arandomclientsecret"
+  return {clientSecret, amount}
+}
 
-//   res.status(StatusCodes.CREATED).json(createResponse(order));
-    res.send("create order");
+export async function createOrder(req, res) {
+  const { orderItems, tax, shippingFee } = req.body;
+  if (!orderItems || orderItems.length < 1) {
+    throw new BadRequest("No order items provided");
+  }
+  if (!tax || !shippingFee) {
+    throw new BadRequest("Please provide tax and shipping fee");
+  }
+
+  let orderItemsHolder = [];
+  let subTotal = 0;
+
+  const revampOrderItems = await Promise.all(
+    orderItems.map(async (item) => {
+      const dbProduct = await Product.findOne({ _id: item.product });
+      if (!dbProduct) {
+        throw new NotFoundError(`No product with id ${item.product}`);
+      }
+      const { name, price, image, _id: productId } = dbProduct;
+      const singleOrderItem = {
+        quantity: item.quantity,
+        name,
+        price,
+        image,
+        product: productId,
+      };
+      return singleOrderItem;
+    })
+  );
+
+  subTotal = revampOrderItems.reduce((acc, curr) => {
+    return (acc += curr.price * curr.quantity);console.log(subTotal);
+  }, 0);
+  const total = subTotal + tax + shippingFee
+
+  // GET CLIENT SECRET
+  const paymentIntent  = await fakeStripeApi({
+    amount: total, currency: "NGN"
+  })
+  const order = await Order.create({
+    tax,
+    shippingFee,
+    subTotal,
+    total,
+    orderItems: revampOrderItems,
+    user: req.user.userId,
+    clientSecret: paymentIntent.clientSecret,
+  });
+
+    res.status(StatusCodes.CREATED).json(createResponse(true, {order, clientSecret: order.clientSecret}));
 }
 
 export async function updateOrder(req, res) {
@@ -22,4 +74,3 @@ export async function getSingleOrder(req, res) {
 export async function getAllOrders(req, res) {
   res.send("get all order");
 }
-
--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,36 +1,36 @@
 import { mongoose } from "mongoose";
 
-const singleOrderItem = new mongoose.Schema({
-    name: {type: String, required: true},
-    image: {type: String, required: true},
-    price: {type: Number, required: true},
-    amount: {type: Number, required: true},
-    product: {type: mongoose.Schema.ObjectId, ref: 'Product', required: true}
-})
+const SingleOrderItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  product: { type: mongoose.Schema.ObjectId, ref: "Product", required: true },
+});
 
 const OrderSchema = new mongoose.Schema(
   {
     tax: {
       type: Number,
-      required: true
+      required: true,
     },
     shippingFee: {
       type: Number,
-      required: true
+      required: true,
     },
     subTotal: {
       type: Number,
-      required: true
+      required: true,
     },
     total: {
       type: Number,
-      required: true
+      required: true,
     },
-    orderItems: [],
+    orderItems: [SingleOrderItemSchema],
     status: {
       type: String,
-      default: 'pending',
-      enum: ['pending', 'paid','shipped', 'delivered', 'failed', 'cancelled']
+      default: "pending",
+      enum: ["pending", "paid", "shipped", "delivered", "failed", "cancelled"],
     },
     user: {
       type: mongoose.Schema.ObjectId,
@@ -39,7 +39,7 @@ const OrderSchema = new mongoose.Schema(
     },
     clientSecret: {
       type: String,
-      required: true
+      required: true,
     },
     paymentId: {
       type: String,
@@ -48,6 +48,8 @@ const OrderSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+const SingleOrderItem = mongoose.model("SingleOrderItem", SingleOrderItemSchema);
+
 const Order = mongoose.model("Order", OrderSchema);
 
-export default Order;
+export  { SingleOrderItem, Order };
